d41765678c760a3229a566a281a406d46834e901
Aaron Yu <AaronYXZ@users.noreply.github.com>
2017-11-21 21:06:45 -0500

    Add files via upload

---
+++ 70. Climbing Stairs.py
@@ -0,0 +1,45 @@
+'''
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+
+
+Example 1:
+
+Input: 2
+Output:  2
+Explanation:  There are two ways to climb to the top.
+
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: 3
+Output:  3
+Explanation:  There are three ways to climb to the top.
+
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+'''
+
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        # http://blog.csdn.net/kenden23/article/details/17377869
+        # https://github.com/qiyuangong/leetcode/blob/master/python/070_Climbing_Stairs.py
+        if n == 1:
+            return 1
+        
+        lst = [1,1]
+        for i in range(2, n +1):
+            lst[0], lst[1] = lst[1], lst[0] + lst[1]
+        
+        return lst[1]
+        
